e7a1d0d8c81fdd385f7d02f56904661bf77bdd5a
Eiji Iwazawa <iwazawa@gmail.com>
2013-05-03 17:22:20 +0900

    long title sample was added

--- NavigationMenu/HAViewController.m
+++ NavigationMenu/HAViewController.m
@@ -14,10 +14,12 @@
 {
     [super viewDidLoad];
     if (self.navigationItem) {
-        CGRect frame = CGRectMake(0.0, 0.0, 200.0, self.navigationController.navigationBar.bounds.size.height);
+        CGRect frame = CGRectMake(0.0, 0.0, 240.0, self.navigationController.navigationBar.bounds.size.height);
         SINavigationMenuView *menu = [[SINavigationMenuView alloc] initWithFrame:frame title:@"Menu"];
         [menu displayMenuInView:self.view];
-        menu.items = @[@"News", @"Top Articles", @"Messages", @"Account", @"Settings", @"Return to Initial Value"];
+        menu.items = @[@"News", @"Top Articles", @"Messages", @"Account", @"Settings",
+                       @"Long Long Long Long Long Long Long Long Long String",
+                       @"Return to Initial Value"];
         menu.delegate = self;
         self.navigationItem.titleView = menu;
     }
@@ -27,7 +29,7 @@
 {
     NSLog(@"did selected item at index %d", index);
     NSString *title = nil;
-    if (index < 5) {
+    if (index < 6) {
         title = [menuView.items objectAtIndex:index];
     }
     [menuView setTitle:title];
