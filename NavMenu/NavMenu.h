49cff8eac618aa8e6d3f87e3431ead40c83080dd
yarec <yarec@163.com>
2015-05-14 12:49:06 +0800

    carthage

---
+++ NavMenu/NavMenu.h
@@ -0,0 +1,20 @@
+//
+//  NavMenu.h
+//  NavMenu
+//
+//  Created by r.t on 15/5/14.
+//  Copyright (c) 2015年 Ivan Sapozhnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavMenu.
+FOUNDATION_EXPORT double NavMenuVersionNumber;
+
+//! Project version string for NavMenu.
+FOUNDATION_EXPORT const unsigned char NavMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavMenu/PublicHeader.h>
+
+
+#import "SINavigationMenuView.h"
